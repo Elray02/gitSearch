7072465b2bc13eb6d53cca02993f7acc72c35a02
elray02 <tassofede@gmail.com>
2020-12-30 13:45:32 +0100

    import the browsres module for the animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TuiRootModule } from '@taiga-ui/core';
 import { HomePageComponent } from './components/homePage/homePage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule, TuiRootModule],
+  imports: [BrowserModule, TuiRootModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
