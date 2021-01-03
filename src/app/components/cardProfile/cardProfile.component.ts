78d4a05095a1370dc51a1dfe9bf961d2f944806b
elray02 <tassofede@gmail.com>
2021-01-03 08:28:12 +0100

    implement the input need it

--- src/app/components/cardProfile/cardProfile.component.ts
+++ src/app/components/cardProfile/cardProfile.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserProfile } from 'src/app/model/userProfile.model';
 
 @Component({
-  selector: 'app-cardProfile',
+  selector: 'app-card-profile',
   templateUrl: './cardProfile.component.html',
-  styleUrls: ['./cardProfile.component.less']
+  styleUrls: ['./cardProfile.component.less'],
 })
 export class CardProfileComponent implements OnInit {
+  @Input() user: UserProfile;
+  @Input() showScheleton: boolean;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
