dbce142800efe4c9909541450d914ca63941be24
elray02 <tassofede@gmail.com>
2021-01-03 07:38:12 +0100

    create service for get the users and handle herror

--- src/app/services/gitSearch.service.ts
+++ src/app/services/gitSearch.service.ts
@@ -1,16 +1,53 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { GitResponse } from '../model/gitResponse.model';
-import { Observable } from 'rxjs';
-
+import { Observable, of } from 'rxjs';
+import { UserProfile } from '../model/userProfile.model';
+import { catchError } from 'rxjs/operators';
+import { TuiNotification, TuiNotificationsService } from '@taiga-ui/core';
 @Injectable({
   providedIn: 'root',
 })
 export class GitSearchService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(TuiNotificationsService)
+    private readonly notificationsService: TuiNotificationsService
+  ) {}
 
-  gitUserSearch = (query: string): Observable<GitResponse> => {
-    const url = `https://api.github.com/search/repositories?q=${query}`;
-    return this.http.get<GitResponse>(url);
-  };
+  gitUserSearch(query: string): Observable<GitResponse> {
+
+    console.log(query);
+    const url = `https://api.github.com/search/users?q=${query}+sort:followers+followers:>1000`;
+    return this.http
+      .get<GitResponse>(url)
+      .pipe(catchError(this.handleError<any>('search for user')));
+  }
+
+  gitUserInfomation(url: string): Observable<UserProfile> {
+    return this.http
+      .get<UserProfile>(url)
+      .pipe(catchError(this.handleError<any>('get user info')));
+  }
+
+
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      this.showNotification(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  showNotification(message: string) {
+    this.notificationsService
+      .show(`Ups this happen: ${message}`, {
+        label: 'What a pity!',
+        status: TuiNotification.Error,
+        autoClose: false,
+      })
+      .subscribe();
+  }
 }
