2369a6e66997885ba55103af052b5f763d104330
elray02 <tassofede@gmail.com>
2021-01-03 08:10:45 +0100

    create the user profile card

---
+++ src/app/components/cardProfile/cardProfile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardProfile',
+  templateUrl: './cardProfile.component.html',
+  styleUrls: ['./cardProfile.component.less']
+})
+export class CardProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
