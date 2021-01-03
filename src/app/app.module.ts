0a6428d6217707de3c7da2261bd63917d0aa6c1f
elray02 <tassofede@gmail.com>
2021-01-03 07:43:46 +0100

    update the main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TuiRootModule } from '@taiga-ui/core';
+import {
+  TuiButtonModule,
+  TuiLoaderModule,
+  TuiModeModule,
+  TuiNotificationsModule,
+  TuiRootModule,
+} from '@taiga-ui/core';
 import { HomePageComponent } from './components/homePage/homePage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  TuiAvatarModule,
+  TuiInputModule,
+  TuiIslandModule,
+} from '@taiga-ui/kit';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { iconsPathFactory, TUI_ICONS_PATH } from '@taiga-ui/core';
+import { AuthInterceptor } from './services/auth.interceptor';
+import { TuiAutoFocusModule } from '@taiga-ui/cdk';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule, TuiRootModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    TuiRootModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TuiInputModule,
+    TuiIslandModule,
+    TuiModeModule,
+    TuiButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TuiNotificationsModule,
+    TuiLoaderModule,
+    TuiAutoFocusModule,
+    TuiAvatarModule,
+  ],
+  providers: [
+    {
+      provide: TUI_ICONS_PATH,
+      useValue: iconsPathFactory('assets/taiga-ui/icons/'),
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
