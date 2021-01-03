c49511053a0f983923ce9396917fec582218de61
elray02 <tassofede@gmail.com>
2021-01-03 08:08:23 +0100

    emit the searched value

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.less']
+  styleUrls: ['./search.component.less'],
 })
 export class SearchComponent implements OnInit {
+  inputUser = '';
 
-  constructor() { }
+  @Output() userResearch: EventEmitter<string> = new EventEmitter();
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  searchValue() {
+    this.userResearch.emit(this.inputUser);
   }
-
 }
